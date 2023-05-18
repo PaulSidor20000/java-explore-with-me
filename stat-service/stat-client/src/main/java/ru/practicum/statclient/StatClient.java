c9677ff5901cdb78a44193f037a46c3af564aa95
PaulSidor20000 <pavel.sidoroff2014@yandex.ru>
2023-05-18 15:48:09 +0300

    add structure to project

---
+++ stat-service/stat-client/src/main/java/ru/practicum/statclient/StatClient.java
@@ -0,0 +1,11 @@
+package ru.practicum.statclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatClient {
+    public static void main(String[] args) {
+        SpringApplication.run(StatClient.class, args);
+    }
+}
